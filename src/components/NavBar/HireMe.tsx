9687d939497a7c1fd4b775365065067e8a86828c
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-04-24 16:53:08 +0300

    button fix

--- src/components/NavBar/HireMe.tsx
+++ src/components/NavBar/HireMe.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { CircularText } from '@/components/Icons'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const MyComponent = () => {
+const HireMe = () => {
+
     return (
-        <div className={'fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0'}>
-            <div className={'w-48 h-auto flex items-center justify-center relative md:w-24'}>
+        <div className={'fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute sm:right-0 z-50'}>
+            <div className={'w-48 h-auto flex items-center justify-center relative md:w-24'} >
                 <CircularText className={'fill-dark animate-spin-slow dark:fill-light'} />
                 <Link href={'mailto:goshagriboedskii@gmail.com'}
+
                       className={'flex items-center justify-center ' +
                           'absolute left-1/2 top-1/2 -translate-x-1/2 ' +
                           '-translate-y-1/2 bg-dark text-light shadow-md ' +
@@ -22,4 +25,4 @@ const MyComponent = () => {
     )
 }
 
-export default MyComponent
+export default HireMe
